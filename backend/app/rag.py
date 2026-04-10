e51ae4e4dbac0beaf65cc7f0e4e079f8eb7e86bf
Naina-Shukla <shuklanaina1111@gmail.com>
2026-04-10 18:30:09 +0530

    Remove torch reduce image size

--- backend/app/rag.py
+++ backend/app/rag.py
@@ -2,7 +2,7 @@ import os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
